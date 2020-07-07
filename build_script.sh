e8c31ef7800c88204aec81fdbb4a1b2974081089
grcann88 <grcann88@gmail.com>
2020-07-07 09:47:00 +0100

    LAMP stack

--- build_script.sh
+++ build_script.sh
@@ -1,3 +1,8 @@
 ls -la;
-sudo yum install git -y;
-git version
+sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+cat /etc/system-release
+sudo yum install -y httpd mariadb-server
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl is-enabled httpd
+
