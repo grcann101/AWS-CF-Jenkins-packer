9ae9afcc5d720043a30e7c62202dd20cb0abd6f9
grcann88 <grcann88@gmail.com>
2020-07-08 15:20:59 +0100

    tf

--- terraformcode/security-group.tf
+++ terraformcode/security-group.tf
@@ -3,9 +3,9 @@
 module "server_sg" {
   source = "terraform-aws-modules/security-group/aws//modules/http-80"
 
-  name        = $rancher-name 
+  name        = var.rancher-name 
   description = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = $vpc-id 
+  vpc_id      = var.vpc-id 
 
-  ingress_cidr_blocks = [$rancher-vpc-cidr,]
+  ingress_cidr_blocks = [var.rancher-vpc-cidr,]
 }
